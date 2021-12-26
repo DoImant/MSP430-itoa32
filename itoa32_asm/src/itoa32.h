1a497f6b8aeb5fd999151ba471231d3803abea46
Kai R. <k@pekaru.de>
2021-12-26 20:02:25 +0100

    Unnecessary definitions deleted

--- itoa32_asm/src/itoa32.h
+++ itoa32_asm/src/itoa32.h
@@ -7,11 +7,8 @@
 extern "C" {
 #endif //__cplusplus
 
-//#define BASE 10U
-//#define BUFFER_SIZE_L 11U
-
-extern char* itoa32(char* const buffer, int32_t val);
-extern char* uitoa32(char* const buffer, uint32_t val);
+extern char* itoa32(char* const strBuf, int32_t val);
+extern char* uitoa32(char* const strBuf, uint32_t val);
 extern void strrev(char* pLeft, uint8_t r_len);
 
 #ifdef __cplusplus
